fec3558da352c0c509255bf9ece07d93f4854860
Nadav Gad <35539676+Nadavgad@users.noreply.github.com>
2024-01-02 13:30:01 +0200

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,25 +1,20 @@
-"""
-Class Summary :
-
-Player constructor get:
-
-name - The player name.
-
-card_counter - Initialized by `default` The player who won the round his own card_counter + 1.
-
-cards - Array that have all the player cards after sharing the cards to players
-
-add_card - function that add card to cards player array
-
-"""
-
 
 class Player:
 
     def __init__(self, name):
+        """
+        :param
+        name: The player name.
+        win_card_counter - Initialized by `default` The player who won the round his own card_counter + 1.
+        cards - Array that have all the player cards after sharing the cards to players
+        """
         self.name = name
-        self.round_counter = 0
+        self.win_round_counter = 0
         self.cards = []
 
     def add_card(self, card):
+        """
+        :param card: card we want to add
+        :return:  card to cards player array
+        """
         self.cards.append(card)
